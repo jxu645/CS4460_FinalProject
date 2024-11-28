b328f7caa9b017eaff6873f4a1de9cfaed3b17b9
jxu645 <jessiexu0422@gmail.com>
2024-11-28 10:11:58 -0500

    not completed bars

--- main.js
+++ main.js
@@ -1,14 +1,66 @@
 var width = 500;
 var height = 500;
 
-var width2 = 900;
+var width2 = 1200;
 var svg = d3.select('svg');
 
-var padding = { t: 60, r: 40, b: 30, l: 120 };
+var padding = { t: 60, r: 40, b: 30, l: 300 };
 
 // Compute chart dimensions
 var barChartWidth = width2 - padding.l - padding.r;
 var chartHeight = height - padding.t - padding.b;
+var barChartHeight = 625;
+
+var colleges;
+
+var barSpacing = 5; // Space between bars
+var barHeight = 10;  // Fixed height for each bar
+var barBand = chartHeight / 409;
+
+// Set up axis
+var xScale = d3.scaleLinear().range([0, barChartWidth]);
+//var yScale = d3.scaleBand().domain(csv.map(d => d.Name)).range([0, csv.length * barSpacing]).padding(0.3);
+
+var xAxisTop = d3.axisTop(xScale).ticks(10).tickFormat(d => d3.format(".0f")(d / 1000) + "k");
+var xAxisBottom = d3.axisBottom(xScale).ticks(10).tickFormat(d => d3.format(".0f")(d / 1000) + "k");
+
+// Chart svg
+var chart1 = d3
+    .select("#chart1")
+    .append("svg")
+    .attr("id", "svg1")
+    .attr("width", width2)
+    .attr("height", 750);
+
+// Chart group for appending chart elements
+var chartGroup = chart1.append("g")
+    .attr("transform", `translate(${padding.l},${padding.t})`);
+
+// Add axis
+chart1.append("g")
+    .attr("class", "xTopAxis")
+    .attr("transform", `translate(${padding.l},${padding.t})`)
+
+chart1.append("g")
+    .attr("class", "xBottomAxis")
+    .attr("transform", `translate(${padding.l},${barChartHeight + padding.t})`)
+
+// Chart title
+chart1.append("text")
+    .attr("x", width2 / 2)
+    .attr("y", padding.t / 2 - 15)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .style("font-weight", "bold")
+    .text("Average Cost of Colleges");
+
+// Axis label
+chart1.append("text")
+    .attr("x", width2 / 2 + 150)
+    .attr("y", barChartHeight + padding.t + 40)
+    .attr("text-anchor", "middle")
+    .style("font-size", "12px")
+    .text("Average Cost per Year");
 
 d3.csv("colleges.csv").then(function (csv) {
     csv.forEach(d => {
@@ -19,271 +71,85 @@ d3.csv("colleges.csv").then(function (csv) {
         d.SATScore = +d["SAT Average"]; // Column: "SAT Average" -> convert to numeric
     });
 
-    var chart2 = d3
-    .select("#chart2")
-    .append("svg:svg")
-    .attr("id", "svg2")
-    .attr("width", width)
-    .attr("height", height);
+    colleges = csv
 
+    xScale.domain([0, d3.max(colleges, function (d) { return d.Cost; })]);
 
+    chart1.select('.xTopAxis')
+        .call(xAxisTop)
+        .selectAll("text")
+        .style("text-anchor", "start")
+        .attr("transform", "translate(-6, -5)");
 
-/**
-Detials on demand (hovering? Clicking?)
-Acceptance rate vs SAT Score
-Dots are colored based on region
-*/
+    chart1.select('.xBottomAxis')
+        .call(xAxisBottom)
+        .selectAll("text")
+        .style("text-anchor", "start")
+        .attr("transform", "translate(-6, 5)");
 
-    function scaleAccept(AcceptanceRate) { // formerlly attack
-        return acceptScale(AcceptanceRate);
-    }
-
-    function scaleSAT(SATScore) { //formerly defense
-        return satScale(SATScore);
-    }
-/*
-    function scaleSpeed(speed) {
-        return speedScale(speed);
-    }*/
-    
-
-    // **** Start of Code for creating scales for axes and data plotting****
-
-    var acceptScale = d3.scaleLinear()
-        .domain(d3.extent(csv, d => d.AcceptanceRate))
-        .range([50, 450]);
-
-    var satScale = d3.scaleLinear()
-        .domain(d3.extent(csv, d => d.SATScore))
-        .range([450, 50])
-
-    // Scale for the speed attribute, mapping to a radius range
-   /* var speedScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.speed))
-        .range([3, 10]);*/
-
-    var svg = d3.select('svg');
-
-    
-
-    // **** End of Code for creating scales for axes and data plotting****
-
-    var tooltip = d3.select('#chart2')
-        .append('div')
-		.attr('class', 'tooltip')
-		.style('position', 'absolute')
-		.style('background-color', 'white')
-		.style('border', '1px solid black')
-		.style('padding', '5px')
-		.style('border-radius', '5px')
-		.style('opacity', 0); // Start with opacity 0 to keep it hidden
-
-    // X-axis - Append to svg (axis and label)
-
-    var x_axis = d3.axisBottom(acceptScale); 
-
-    chart2.append('g')
-    .attr("width", width) 
-    .attr("height", height)
-    .attr('class', 'x axis')
-    .attr("transform", "translate(0,450)") 
-    .call(x_axis);
-
-    chart2.append("text")
-    .attr('class', 'x axis')
-    .attr("transform", "translate(225,475)") 
-    .text("Acceptance Rate");
-
-    
-
-    // Y-axis - Append to svg (axis and label)
-
-    
-    var y_axis = d3.axisLeft(satScale); 
-
-    chart2.append('g')
-    .attr("width", width) 
-    .attr("height", height)
-    .attr('class', 'y axis')
-    .attr("transform", "translate(50,0) ") 
-    .text("y Axis")
-    //.attr("transform", "rotate(90)") 
-     .call(y_axis) ;
-
-     chart2.append("text")
-     .attr('class', 'y axis')
-     .attr("transform", "rotate(90) translate(170, 0) ")
-     .text("SAT Average");
-
-    // Title - Append to svg
-
-    
-    chart2.append("text")
-    .attr("transform", "translate(175,40)") 
-    .text("Colleges Acceptance Rate vs SAT Average");
-    
-
-    // Plot the points & scale radius by speed - Enter and append
-    var highSpeed = 100;
-    chart2.append('g')
-    .selectAll("dot")
-    .data(csv)
-    .enter()
-
-    .append("circle")
-    .attr("cx", function (d) { return scaleAccept(d.AcceptanceRate); } )
-    .attr("cy", function (d) { return scaleSAT(d.SATScore); } )
-    .attr("r", 5)
-      //.attr("r", function (d) { return scaleSpeed(d.Speed); })
-    //.style("fill" ,"steelblue")
-  //  .style('fill', function(d) {
-    //    return d.Speed > highSpeed ? '#FFD700' : 'steelblue';
-   // })
-    //.style("opacity" ,"0.7")
-    .on('mouseover', function(event, i) {
-        const a = csv[i]; // Use 'i' as an index to access the correct object
-        //console.log(csv)
-        //console.log(i)
-        //console.log(a)
-        const cx = scaleAccept(i["Admission Rate"]);// TO-DO: Get the x-position for the tooltip
-        const cy = scaleSAT(i["SAT Average"]);// TO-DO: Get the y-position for the tooltip
-        //const pointer = d3.pointer(event);
-        // TO-DO: Style the tooltip correctly.
-        var htmlText = "";
-       // if (d["Type 2"] == '') {
-        htmlText = "<b>" + i.Name + "</b><br\>" + "Cost: $" + i.Cost;
-      //  } else {
-           // htmlText = "<b>" + d.Name + "</b><br\>" + "Type 1: " + d["Type 1"] + "<br\>" + "Type 2: " + d["Type 2"];
-       // }
-       // console.log(event.pageX)
-        tooltip
-        .attr("class", "tooltip")
-        .html(`${htmlText}`)
-        .style("Left", event.pageX+25 + "px")
-        .style("Top", event.pageY-35 + "px")
-        .style("opacity", "1.0")
-        .style("position", "absolute")
-    }).on('mouseout', function() {
-        // TO-DO: Hide the tooltip when not hovering
-        return tooltip.style("opacity", "0.0");
-    })
-    .attr("class", d => {
-        if (d.Region == "Outlying Areas"){
-            return "outlying";
-        } else if (d.Region == "New England") {
-            return "newengland";
-        } else if (d.Region == "Mid-Atlantic") {
-            return "midatlantic";
-        } else if (d.Region == "Southeast") {
-            return "southeast";
-        } else if (d.Region == "Great Lakes") {
-            return "greatLakes";
-        } else if (d.Region == "Great Plains") {
-            return "greatPlains";
-        } else if (d.Region == "Rocky Mountains") {
-            return "rocky";
-        } else if (d.Region == "Southwest") {
-            return "southwest";
-        } else {
-            return "farWest";
-        } 
-    });
-
-
-    d3
-    .select("#newEngland")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "newengland")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#midAtlantic")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "midatlantic")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#southEast")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "southeast")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#greatLakes")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "greatLakes")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#greatPlains")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "greatPlains")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#rockY")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "rocky")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#southWest")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "southwest")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#farWest")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "farWest")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-    d3
-    .select("#outLying")
-    .append("circle")
-    .attr("cx", 6 )
-    .attr("cy", 6)
-    .attr("r", 5)
-    .attr("class", "outlying")
-    .attr("stroke-width", 1)
-    .attr("stroke", "black");
-
-
-
-
-
-    console.log(csv);
+    updateChart('all-regions');
+    //console.log(csv);
 })
 
+// Update Chart
+function updateChart(filterKey) {
+    // Load and filter the data based on the filterKey (for now, no filtering)
+    var filteredRegions;
+
+    if (filterKey === 'all-regions')
+        filteredRegions = colleges.filter(d => d.Region !== filterKey);
+    else filteredRegions = colleges.filter(d => d.Region === filterKey);
+
+    const totalHeight = filteredRegions.length * (barHeight + barSpacing) + 20;
+
+    // Set the SVG height to fit all bars
+    d3.select('#chart1')
+        .attr('width', width2 - padding.r)
+        .attr('height', totalHeight);
+
+    var bars = chartGroup.selectAll('.bar')
+        .data(filteredRegions, d => d.Name);
+    //enter
+    bars.enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('height', barHeight)
+        .attr('fill', 'darkblue')
+        .attr('width', d => xScale(d.Cost))
+        .attr('y', (d, i) => {
+            const yPosition = i * (barHeight + barSpacing) + 10;
+            return yPosition < totalHeight ? yPosition : -9999;
+        });
+    //update
+    bars.attr('width', d => xScale(d.Cost))
+        .attr('y', (d, i) => {
+            const yPosition = i * (barHeight + barSpacing) + 10;
+            return yPosition < totalHeight ? yPosition : -9999;
+        });
+    //exit
+    bars.exit().remove();
+
+    // Text labels for bars
+    var labels = chartGroup.selectAll('.label')
+        .data(filteredRegions, d => d.Name);
+    //enter
+    labels.enter()
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -5)
+        .attr('dy', '0.3em')
+        .attr('text-anchor', 'end')
+        .attr('y', (d, i) => {
+            const yPosition = i * (barHeight + barSpacing) + 15;
+            return yPosition < barChartHeight ? yPosition : -9999;
+        })
+        .text(d => d.Name);
+    //update
+    labels.attr('y', (d, i) => {
+        const yPosition = i * (barHeight + barSpacing) + 15;
+        return yPosition < barChartHeight ? yPosition : -9999;
+    })
+        .text(d => d.Name);
+    //exit
+    labels.exit().remove();
+}
